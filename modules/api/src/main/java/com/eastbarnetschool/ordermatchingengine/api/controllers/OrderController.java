707f82a185d38c8665738425be2ef5c1f1f56ad7
Albert Onea <albert.onea@gmail.com>
2024-11-30 15:02:46 +0000

    postgres docker compose

--- modules/api/src/main/java/com/eastbarnetschool/ordermatchingengine/api/controllers/OrderController.java
+++ modules/api/src/main/java/com/eastbarnetschool/ordermatchingengine/api/controllers/OrderController.java
@@ -2,17 +2,13 @@ package com.eastbarnetschool.ordermatchingengine.api.controllers;
 
 import com.eastbarnetschool.ordermatchingengine.domain.Order;
 import com.eastbarnetschool.ordermatchingengine.domain.OrderGateway;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 //@RestController()
 //@RequestMapping(value = "/order")
 @Controller
-@Slf4j
 public class OrderController {
     private final OrderGateway orderGateway;
 
@@ -27,13 +23,13 @@ public class OrderController {
 //    }
 
     @MessageMapping("/order.hello")
-    @SendTo("/stream")
+    @SendTo("/stream/aggTrade")
     public String hello(String message) {
         return "hello" + message;
     }
 
     @MessageMapping("/order.place")
-    @SendTo("/stream")
+    @SendTo("/stream/trade")
     public OrderRequest placeOrder(OrderRequest order) {
         orderGateway.placeOrder(new Order(order.getPrice(), order.getQuantity(), order.getUsername(), order.getSide(), order.getOrderType()));
         return order;
